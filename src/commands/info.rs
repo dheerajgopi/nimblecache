f49de01d07bf9e877f8af2f7323ad80c39587f36
Dheeraj Gopinath <dheerajgopinath@gmail.com>
2024-07-07 13:36:15 +0530

    feat: Modify INFO command to support optional parameters
    Optional parameters can be used to return specific sections of the server info.
    Currently support "replication" parameter only.

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -1,12 +1,70 @@
 use crate::commands::traits::CommandExecutor;
 use crate::protocol::resp::types::RespType;
+use crate::protocol::resp::types::RespType::SimpleError;
+
+use anyhow::{anyhow, Result};
+
+const ALL_INFO_ARGS: [InfoArg; 1] = [InfoArg::REPLICATION];
 
 /// Struct for the INFO command.
 pub struct Info {}
 
 impl CommandExecutor for Info {
     /// Returns the server info in BulkString format.
-    fn execute(&mut self, _: &[&RespType]) -> RespType {
-        return RespType::BulkString("# Server\n\nnimblecache_version: 0.1.0".into());
+    /// Specific sections of info can be selected by specifying optional parameters.
+    /// If no optional parameters are specified, all sections are returned.
+    ///
+    /// # Supported optional params
+    /// - replication : Master/replica replication information.
+    fn execute(&mut self, args: &[&RespType]) -> RespType {
+        let mut info_args = vec![];
+
+        // get the info sections to be returned
+        if args.len() == 0 {
+            info_args.extend(ALL_INFO_ARGS);
+        } else {
+            for arg in args {
+                let info_arg = InfoArg::parse(*arg);
+                match info_arg {
+                    Ok(ia) => {
+                        info_args.push(ia);
+                    }
+                    Err(e) => return SimpleError(format!("{}", e)),
+                }
+            }
+        }
+
+        // append section infos in a loop
+        let mut info = String::new();
+
+        for info_arg in info_args {
+            let section = match info_arg {
+                InfoArg::REPLICATION => "# Replication\nrole:master\n",
+            };
+
+            info.push_str(section)
+        }
+
+        return RespType::BulkString(info);
+    }
+}
+
+/// Arguments supported by the INFO command.
+enum InfoArg {
+    /// Info about replication.
+    REPLICATION,
+}
+
+impl InfoArg {
+    fn parse(arg: &RespType) -> Result<InfoArg> {
+        let s = match arg {
+            RespType::BulkString(s) => s,
+            _ => return Err(anyhow!("ERR Invalid argument. Value must be a bulk string")),
+        };
+
+        match s.to_uppercase().as_str() {
+            "REPLICATION" => Ok(InfoArg::REPLICATION),
+            _ => Err(anyhow!("Invalid argument for INFO command")),
+        }
     }
 }
