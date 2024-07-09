9dc3229a60dd0ac9a259e50e76abcf1fb424eb6e
Dheeraj Gopinath <dheerajgopinath@gmail.com>
2024-07-09 15:07:08 +0530

    feat: Set master replication id and offset when starting the server by assuming master role
    The master replication id will be a pseudo-random alphanumeric string.

--- src/server/info.rs
+++ src/server/info.rs
@@ -1,4 +1,5 @@
 use core::fmt;
+use rand::distributions::{Alphanumeric, DistString};
 
 /// Struct to store server information
 pub struct ServerInfo {
@@ -7,21 +8,31 @@ pub struct ServerInfo {
 
 impl ServerInfo {
     pub fn new(role: Role) -> ServerInfo {
-        ServerInfo { role: role }
+        ServerInfo { role }
     }
+}
+
+/// Wrapper for storing master replication id and offset
+pub struct Master {
+    replication_id: String,
+    replication_offset: u16,
+}
+
+pub struct Slave {
+    replica_of: String,
 }
 
 /// Role assumed by the server
 pub enum Role {
     /// Master role.
-    MASTER,
+    MASTER(Master),
     /// Slave role. Contains the master name of whom the instance is a replica of.
-    SLAVE(String),
+    SLAVE(Slave),
 }
 
 impl Role {
     /// Parse role based on the string value.
-    /// Anything other than "master" (case insensitive) will be considered as slave.
+    /// Anything other than "master" (case-insensitive) will be considered as slave.
     pub fn from_str(s: &str) -> Role {
         match s.to_uppercase().as_str() {
             "MASTER" => Self::as_master(),
@@ -36,8 +47,10 @@ impl Role {
         s.push_str("role:");
 
         match self {
-            Role::MASTER => {
-                s.push_str("master");
+            Role::MASTER(m) => {
+                s.push_str("master\n");
+                s.push_str(format!("master_replid:{}\n", m.replication_id).as_str());
+                s.push_str(format!("master_repl_offset:{}\n", m.replication_offset).as_str())
             }
             Role::SLAVE(_) => {
                 s.push_str("slave");
@@ -47,20 +60,27 @@ impl Role {
         s.to_string()
     }
 
+    /// Create master role, generate a pseudo-random alphanumeric replication id, and set that to the role.
+    /// replication offset will be set as 0.
     fn as_master() -> Role {
-        Role::MASTER
+        let replication_id = Alphanumeric.sample_string(&mut rand::thread_rng(), 40);
+        Role::MASTER(Master {
+            replication_id,
+            replication_offset: 0,
+        })
     }
 
+    /// Create slave role.
     fn as_slave_of(master: String) -> Role {
-        Role::SLAVE(master)
+        Role::SLAVE(Slave { replica_of: master })
     }
 }
 
 impl fmt::Display for Role {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Role::MASTER => f.write_str("master"),
-            Role::SLAVE(m) => f.write_fmt(format_args!("slave of {}", m)),
+            Role::MASTER(_) => f.write_str("master"),
+            Role::SLAVE(m) => f.write_fmt(format_args!("slave of {}", m.replica_of)),
         }
     }
 }
