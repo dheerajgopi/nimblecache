19dace51d3a97fc435743c6e66de878c0c112dd2
Dheeraj Gopinath <dheerajgopinath@gmail.com>
2024-08-09 12:41:50 +0530

    feat: Allow setting the listener port via CLI parameter

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,42 @@ mod command;
 mod handler;
 mod resp;
 mod server;
+mod storage;
 
 use crate::server::Server;
 use anyhow::Result;
+use clap::Parser;
 use log::{error, info};
 use tokio::net::TcpListener;
+
+const DEFAULT_PORT: u16 = 6379;
+
+#[derive(Debug, Parser)]
+#[command(
+    name = "nimblecache-server",
+    version,
+    author,
+    about = "A RESP based in-memory cache"
+)]
+struct Cli {
+    #[arg(long)]
+    port: Option<u16>,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
 
-    let addr = format!("127.0.0.1:{}", 6380);
+    let cli = Cli::parse();
+    let port = cli.port.unwrap_or(DEFAULT_PORT);
+
+    let addr = format!("127.0.0.1:{}", port);
     let listener = match TcpListener::bind(&addr).await {
         Ok(tcp_listener) => tcp_listener,
         Err(e) => panic!("Could not bind the TCP listener to {}. Err: {}", &addr, e),
     };
 
-    info!("Started TCP listener on port {}", 6380);
+    info!("Started TCP listener on port {}", port);
 
     let mut server = Server::new(listener);
     tokio::select! {
